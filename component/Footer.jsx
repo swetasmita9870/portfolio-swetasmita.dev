ce30619bfeed0bd6ef21ab1cc3e6a35fe24f0d6c
Swetasmita Ranjan Sahoo <swetasmita@appscrip.co>
2023-04-16 04:27:41 +0530

    Change footer

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -1,35 +1,28 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Link } from "@material-ui/core";
-import {
-  Facebook as FacebookIcon,
-  Instagram as InstagramIcon,
-  LinkedIn as LinkedInIcon,
-  Twitter as TwitterIcon,
-  WhatsApp,
-} from "@material-ui/icons";
+// import { makeStyles } from "@material-ui/core/styles";
+// import { Grid, Link } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: "#f7f7f7",
-    padding: theme.spacing(1),
-  },
-  socialIcons: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap:"wrap",
-    "& > *": {
-      margin: theme.spacing(2),
-    },
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     backgroundColor: "#f7f7f7",
+//     padding: theme.spacing(1),
+//   },
+//   socialIcons: {
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     flexWrap:"wrap",
+//     "& > *": {
+//       margin: theme.spacing(2),
+//     },
+//   },
+// }));
 
 export default function Footer() {
   const classes = useStyles();
   return (
     <div className={`${classes.root} bg-dark`}>
-        <h2 className="text-center primary-text text-uppercase">Contact</h2>
+      {/* <h2 className="text-center primary-text text-uppercase">Contact</h2>
       <Grid container direction="row" justify="center" alignItems="center">
         <Grid item xs={12}>
           <div className={classes.socialIcons}>
@@ -58,12 +51,12 @@ export default function Footer() {
               <WhatsApp style={{color:"#25D366"}} />
             </Link>
 </div>
-          
-            
-           
+
+
+
           </div>
         </Grid>
-      </Grid>
+      </Grid> */}
       <div className="col-12 d-flex align-items-center justify-content-center">
       <p className="text-white text-center textContainer">©Swetasmita Ranjan Sahoo 2023. All Rights Reserved.
 
